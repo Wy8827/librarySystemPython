152aecb8aa0220ef8583230b7eb2c8c962151952
nxyan2007 <wongw.46252@gmail.com>
2025-10-19 16:52:59 +0800

    Refactor member.py for improved structure and clarity

--- assignment/member.py
+++ assignment/member.py
@@ -1,87 +1,133 @@
-USER_ID = []
-PASSWORD = []
-BOOKNAME = []
-BOOKID = []
-BORROWED_BOOKS = []
+user_id = []
+password = []
+borrowed_books=[]
 
-# Input total users and their details, JUST TO TEST WHETHER CODE WORKS
-# FUNCTION NOT INCLUDED IN THE QUESTION BUT NEED TO USE 
-USER_TOTAL = int(input("Please input total number of users: "))
-for i in range(USER_TOTAL):
-    user_id = int(input("Enter User ID: "))
-    password = input("Enter Password: ")
-    USER_ID.append(user_id)
-    PASSWORD.append(password)
+users = [
+    {"user_id": 1, "password": "123"},
+    {"user_id": 2, "password": "123"}
+]
 
-# Input total books and their details
-BOOKCOUNT = int(input("Please input total number of books: "))
-for i in range(BOOKCOUNT):
-    book_name = input("Enter Book Name: ")
-    book_id = int(input("Enter Book ID: "))
-    BOOKNAME.append(book_name)
-    BOOKID.append(book_id)
+book = [
+    {"book_name": "name1", "book_id": 123, "number_of_book_available": 2},
+    {"book_name": "name2", "book_id": 1234, "number_of_book_available": 0},
+    {"book_name": "name3", "book_id": 12345, "number_of_book_available": 1},
+    {"book_name": "name4", "book_id": 123456, "number_of_book_available": 3},
+]
 
-# User login
-USER_ID_TEMP = int(input("Please Input User ID: "))
-PASSWORD_TEMP = input("Please Input Password: ")
 
-FLAG = False
+def loginpage():
+    USER_ID_TEMP = int(input("Please Input User ID: "))
+    PASSWORD_TEMP = input("Please Input Password: ")
 
-for i in range(USER_TOTAL):
-    if USER_ID_TEMP == USER_ID[i] and PASSWORD_TEMP == PASSWORD[i]:
-        print("Login Successful!")
-        FLAG = True
-        break
+    login_success = False
 
-if FLAG == False:
-    print("ERROR: Invalid ID or Password.")
+    for i in range(len(users)):
+        if USER_ID_TEMP == users[i]["user_id"] and PASSWORD_TEMP == users[i]["password"]:
+            print("Login Successful!")
+            login_success = True
+            member_interface()
+            break
 
-# Main menu (runs as long as login is successful)
-BORROWED_COUNT = 0
+    if not login_success:
+        print("ERROR: Invalid ID or Password.")
+        return loginpage()  # <--- call itself again to retry
 
-while FLAG == True:
-    print("-----------------------------------")
-    print("1. Search by Book Name")
-    print("2. Search by Book ID")
-    print("3. View Borrowed History")
-    print("-----------------------------------")
-    CHOICE = int(input("Enter your choice: "))
 
-    if CHOICE == 1:
-        BOOKNAME_TEMP = input("Please input book name: ")
-        FOUND = False
+def member_interface():
+    print('             "member"               ')
+    try:
+        xuan = int(input("-----------------------------------\n1. Search by Book Name\n2. Search by Book ID\n3. View Borrowed History\n4. Log Out\n-----------------------------------\nPlease Input Choice : "))
+    except ValueError:
+        print("Invalid input! Please enter a number.")
+        return member_interface()  # go back if not a number
 
-        for i in range(BOOKCOUNT):
-            if BOOKNAME_TEMP == BOOKNAME[i]:
-                print("You have borrowed:", BOOKNAME[i])
-                BORROWED_BOOKS.append(BOOKNAME[i])
-                BORROWED_COUNT += 1
-                FOUND = True
-
-        if not FOUND:
-            print("Book not found.")
-
-    elif CHOICE == 2:
-        BOOKID_TEMP = int(input("Please input book ID: "))
-        FOUND = False
-
-        for i in range(BOOKCOUNT):
-            if BOOKID_TEMP == BOOKID[i]:
-                print("You have borrowed:", BOOKNAME[i])
-                BORROWED_BOOKS.append(BOOKNAME[i])
-                BORROWED_COUNT += 1
-                FOUND = True
-
-        if not FOUND:
-            print("Book ID not found.")
-
-    elif CHOICE == 3:
-        print("Your Borrowed Books:")
-        if BORROWED_COUNT == 0:
-            print("No books borrowed yet.")
-        else:
-            for i in range(BORROWED_COUNT):
-                print(f"{i + 1}. {BORROWED_BOOKS[i]}")
-
+    if xuan == 4:
+        print("Goodbye!")
+    elif xuan in [1, 2, 3]:
+        member_interface_xuan(xuan)
     else:
-        print("Please input 1, 2, or 3 only!")
+        print("Invalid choice, please try again.")
+        return member_interface()  # go back again
+
+
+def member_interface_xuan(xuan):
+    if xuan == 1:
+        searchbookname()
+    elif xuan == 2:
+        searchbookid()
+    elif xuan == 3:
+        borrowhistory()
+
+
+def searchbookname():
+    bookname = input("Please enter book name: ")
+    print(f"Searching for '{bookname}' ...")
+
+    found_book = False
+    for x in range(len(book)):
+        if bookname == book[x]["book_name"]:
+            found_book = True
+            if book[x]["number_of_book_available"] > 0:
+                CHOICE = int(input("Book available\n-----------------------------------\n1. Borrow\n2. Exit\nEnter your choice: "))
+                if CHOICE == 1:
+                    print(f"You have borrowed '{book[x]['book_name']}'")
+                    book[x]["number_of_book_available"] -= 1
+                    borrowed_books.append({
+                        "book_name": book[x]["book_name"],
+                        "book_id": book[x]["book_id"]
+                    })
+                elif CHOICE == 2:
+                    print("Returning to member page...")
+                    return member_interface()
+            else:
+                print("Book is not available right now.")
+            break  # stop searching once book is found
+
+    if not found_book:
+        print("Book not found.")
+        return member_interface()
+    return member_interface()
+
+
+def searchbookid():
+    bookid = int(input("Please enter book ID: "))
+    print(f"Searching for book ID '{bookid}'...")
+
+    found_book = False
+    for x in range(len(book)):
+        if bookid == book[x]["book_id"]:
+            found_book = True
+            if book[x]["number_of_book_available"] > 0:
+                CHOICE = int(input(f"Book available: '{book[x]['book_name']}'\n-----------------------------------\n1. Borrow\n2. Exit\nEnter your choice: "))
+                if CHOICE == 1:
+                    print(f"You have borrowed '{book[x]['book_name']}'")
+                    book[x]["number_of_book_available"] -= 1
+                    borrowed_books.append({
+                        "book_name": book[x]["book_name"],
+                        "book_id": book[x]["book_id"]
+                    })
+                elif CHOICE == 2:
+                    print("Returning to member page...")
+                    book[x]['bookname']['bookid'] 
+                    return member_interface()
+            else:
+                print("Book is not available right now.")
+            break
+
+    if not found_book:
+        print("Book not found.")
+        return member_interface()
+    return member_interface()
+
+
+def borrowhistory():
+    if len(borrowed_books) == 0:
+        print("You have not borrowed any books yet.")
+    else:
+        print("Borrowed Books:")
+        for b in borrowed_books:
+            print(f"- {b['book_name']} (ID: {b['book_id']})")
+
+
+# Start program
+loginpage()
