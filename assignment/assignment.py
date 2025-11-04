8f71dec5c4fa69973257725d304485fa5e5397f0
Wy8827 <ngwanyang827@gmail.com>
2025-11-04 16:53:02 +0800

    Add files via upload

--- assignment/assignment.py
+++ assignment/assignment.py
@@ -15,11 +15,11 @@ def ensure_newline(path):
 def isbn_input():
     while True:
         isbn = input("Enter Book ISBN : ").strip()
-        isbn1 = input("Enter Book ISBN Again to Verify : ").strip()
         if isbn == "0":
-            return None
+            return isbn
+        isbn1 = input("Enter Book ISBN Again to Verify : ").strip()
         if isbn1 == "0":
-            return None
+            return isbn1
         if isbn != isbn1:
             print("ISBN does not match!\n")
             continue
@@ -85,11 +85,11 @@ def login():
     while True:
         try:
             print("=== WELCOME TO LIBRARY SYSTEM ===")
-            print("1. Admin")
+            print("1. Library Admin")
             print("2. Library Staff")
-            print("3. User")
+            print("3. Library Member")
             print("4. Visitor")
-            print("5. Register")
+            print("5. Register New User")
             print("0. Exit")
 
             op1 = input("Enter your choice: ")
@@ -103,14 +103,14 @@ def login():
                 verify_login("staff")
                 continue
             elif op1 == "3":
-                verify_login("user")
+                verify_login("member")
                 continue
             elif op1 == "4":
-                # visitor_menu()
-                return True
+                visitor_menu()
+                continue
             elif op1 == "5":
                 register_user("member")
-                return True
+                continue
             else:
                 print("Invalid choice\n")
                 continue
@@ -156,7 +156,7 @@ def verify_login(role):
                                 return None
                             elif role == "staff":
                                 staff_menu()
-                            elif role == "user":
+                            elif role == "member":
                                 user_menu()
                             return None
             except FileNotFoundError:
@@ -185,7 +185,6 @@ def register_user(role):
 
         user_found = True
         while user_found:
-            # --- NEW: strict username length check (max 10) ---
             while True:
                 new_name = input("Enter username (minimum 3 characters and maximum 10 characters): ").strip()
                 if len(new_name) < 3 or len(new_name) >10:
@@ -213,7 +212,6 @@ def register_user(role):
                             user_found = True
                             break
                     else:
-                        # no break → not found
                         user_found = False
             except FileNotFoundError:
                 print("Account file not found. Creating new one...")
@@ -277,6 +275,8 @@ def addbook():
         print("\n=== ADD BOOK ===")
         print("Type 0 to back to manage book menu!")
         clean_isbn=isbn_input()
+        if clean_isbn == "0":
+            return None
         book_name = input("Enter book name: ").strip()
         while True:
             try:
@@ -353,6 +353,8 @@ def editbook():
                             print("ENTER NEW BOOK DETAIL")
 
                             clean_isbn = isbn_input()
+                            if clean_isbn == "0":
+                                return None
                             new_name = input("Enter book name: ").strip()
                             while True:
                                 try:
@@ -426,5 +428,96 @@ def deletebook():
         print("Books file not found.\n")
     except Exception as e:
         print(f"Error occurred in deletebook(): {e}\n")
+
+# Function: Display the visitor menu
+def visitor_menu():
+    print("\n=== VISITOR MENU ===")
+    print("1. Search book")
+    print("2. Visit all books")
+    print("0. Return to main menu...")
+    # Prompt the user to enter a choice
+    choice = input("Enter your choice: ")
+
+    # Handle user's choice
+    if choice == "1":
+        return search_book()
+    elif choice == "2":
+        return display_book()
+    elif choice == "0":
+        print()
+        return None # Exit the visitor menu
+    else:
+        print("Invalid choice") # Handle invalid input
+        return None
+
+# Function: Search for a book by isbn, title, author, ID, or language
+def search_book():
+    # Convert user input to lowercase for case-insensitive search
+    keyword = input("Enter(Book title or Book ISBN or Author or Language): ").lower()
+    found = False # Track whether a matching book is found
+    try:
+        with open(book_file, "r") as f: # Open the book file for reading
+            record = f.readlines()
+            if not record:
+                print("No records found.\n")
+            # Print header
+            print("=" * 141)
+            print(f"| {'BOOK_ISBN':^20} | {'BOOK TITLE':^50} | {'AUTHOR':^30} | {'LANGUAGE':^12} | {'STATUS':^13} |")
+            print("=" * 141)
+            for line in record: # Loop through each line (book record) in the file
+                book_isbn, title, language, quantity, author = line.strip().split(",")
+                if keyword in title.lower() or keyword in author.lower() or keyword in book_isbn.lower() or keyword in language.lower():
+                    # Determine book availability
+                   if int(quantity) > 0:
+                       status = "Available"
+                       found = True
+                   else:
+                       status = "Not Available"
+                       found = True
+                    # Print each matching book in a formatted table row
+                   print(f"| {book_isbn:^20} | {title:<50} | {author:<30} | {language:<12} | {status:<13} |")
+                   print("=" * 141)
+
+        # If no matching record found, display message
+        if not found:
+            print("\nNo matching books found! Please enter a valid author or book name or book ISBN or language.")
+
+    except FileNotFoundError: # Handle case where the file does not exist
+        print("No records found.\n")
+
+    # Return to visitor menu after search
+    return visitor_menu()
+
+
+# Function: Display all books in the library catalog
+def display_book():
+    print("\n=== Library Catalog ===")
+    try: # Open the book file for reading
+        with open(book_file, "r") as f:
+            record = f.readlines()
+            if not record:
+                print("No records found.\n")
+                return None
+
+            # Print header
+            print("=" * 141)
+            print(f"| {'BOOK_ISBN':^20} | {'BOOK TITLE':^50} | {'AUTHOR':^30} | {'LANGUAGE':^12} | {'STATUS':^13} |")
+            print("=" * 141)
+
+            for line in record: # Loop through each record in the file
+                    book_isbn, title, language, quantity, author = line.strip().split(",")
+                    # Determine book availability
+                    if int(quantity) > 0:
+                        status = "Available"
+                    else:
+                        status = "Not Available"
+            # Print book information in formatted table
+                    print(f"| {book_isbn:^20} | {title:<50} | {author:<30} | {language:<12} | {status:<13} |")
+                    print("=" * 141)
+
+    except FileNotFoundError: # Handle missing book file
+        print("\nNo matching books found! Please enter a valid author or book name.\n")
+    # Return to visitor menu after displaying books
+    return visitor_menu()
 
 login()
